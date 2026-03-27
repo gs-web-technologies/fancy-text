5ef76bfd0a89712df8dd01d6972f562f9d5d8034
Hemlata <payal.gswebtech@gmail.com>
2026-03-27 15:55:42 +0530

    make SignatureTemlateL dyanamic

--- src/components/email-signature/SignatureTemlateL.jsx
+++ src/components/email-signature/SignatureTemlateL.jsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 import React from 'react'
 
-function SignatureTemlateL() {
- return (
+function SignatureTemlateL({ name, email, job_title, phone_no, organization, logo, linkedin, instagram, twitter, facebook }) {
+  const imageSrc = logo && logo.length != 0 ? logo : "/assets/images/avatar.png";
+  return (
     <table
       cellPadding="0"
       cellSpacing="0"
@@ -15,7 +17,7 @@ function SignatureTemlateL() {
       <tbody>
         <tr>
           {/* LEFT: Circular Profile Photo */}
-          <td style={{ verticalAlign: "middle", paddingRight: "20px", width: "120px" }}>
+          <td style={{ background: "linear-gradient(135deg, #f06e6e 20%, #f06e6e 0%, #e99595 80%, #e9dede 20%)", verticalAlign: "middle", padding: "0px 10px 0px 10px", width: "120px" }}>
             <div
               style={{
                 width: "110px",
@@ -26,7 +28,7 @@ function SignatureTemlateL() {
               }}
             >
               <img
-                src="https://via.placeholder.com/110"
+                src={imageSrc}
                 alt="Profile Photo"
                 style={{
                   width: "110px",
@@ -49,7 +51,7 @@ function SignatureTemlateL() {
           />
 
           {/* RIGHT: Info */}
-          <td style={{ verticalAlign: "middle", paddingLeft: "24px" }}>
+          <td style={{ background: "linear-gradient(-135deg, #f06e6e 20%, #f06e6e 0%, #e99595 80%, #e9dede 20%)", verticalAlign: "middle", paddingLeft: "24px" }}>
             <table cellPadding="0" cellSpacing="0">
               <tbody>
                 {/* Name */}
@@ -58,15 +60,16 @@ function SignatureTemlateL() {
                     <p
                       style={{
                         margin: "0 0 2px 0",
+                        padding: "8px 0px 5px 0px",
                         fontSize: "30px",
                         fontWeight: "bold",
-                        color: "#1a1a1a",
+                        color: "#fffcfc",
                         letterSpacing: "0.5px",
                         lineHeight: "1.1",
                         fontFamily: "Georgia, 'Times New Roman', serif",
                       }}
                     >
-                      Alison Lebofsky
+                      {name ? name : 'Your Name'}
                     </p>
                   </td>
                 </tr>
@@ -77,15 +80,15 @@ function SignatureTemlateL() {
                     <p
                       style={{
                         margin: "0",
-                        fontSize: "11px",
+                        fontSize: "14px",
                         fontWeight: "bold",
-                        color: "#1a1a1a",
+                        color: "#0f0e0e",
                         letterSpacing: "1.5px",
                         textTransform: "uppercase",
                         fontFamily: "Arial, sans-serif",
                       }}
                     >
-                      FOUNDER + CREATIVE DIRECTOR
+                      {job_title ? job_title : 'Job Title'}
                     </p>
                   </td>
                 </tr>
@@ -98,13 +101,13 @@ function SignatureTemlateL() {
                         margin: "0",
                         fontSize: "11px",
                         fontWeight: "bold",
-                        color: "#1a1a1a",
+                        color: "#0c0a0a",
                         letterSpacing: "1.5px",
                         textTransform: "uppercase",
                         fontFamily: "Arial, sans-serif",
                       }}
                     >
-                      PERSPETIIV DESIGN CO.
+                      {organization ? organization : 'Organiztion'}
                     </p>
                   </td>
                 </tr>
@@ -115,12 +118,14 @@ function SignatureTemlateL() {
                     <p
                       style={{
                         margin: "0 0 2px 0",
-                        fontSize: "12px",
-                        color: "#444444",
+                        fontSize: "14px",
+                        color: "#0f0f0f",
                         fontFamily: "Arial, sans-serif",
                       }}
                     >
-                      (720) 891-7858
+                      <b>
+                        {phone_no ? phone_no : "(720) 891-7858"}
+                      </b>
                     </p>
                   </td>
                 </tr>
@@ -131,12 +136,14 @@ function SignatureTemlateL() {
                     <p
                       style={{
                         margin: "0 0 2px 0",
-                        fontSize: "12px",
-                        color: "#444444",
+                        fontSize: "14px",
+                        color: "#0a0a0a",
                         fontFamily: "Arial, sans-serif",
                       }}
                     >
-                      alison@perspektivv.com
+                      <b>
+                        {email ? email : "your_email@gmail.com"}
+                      </b>
                     </p>
                   </td>
                 </tr>
@@ -147,12 +154,14 @@ function SignatureTemlateL() {
                     <p
                       style={{
                         margin: "0",
-                        fontSize: "12px",
-                        color: "#444444",
+                        fontSize: "14px",
+                        color: "#0f0909",
                         fontFamily: "Arial, sans-serif",
                       }}
                     >
-                      perspektivv.com
+                      <b>
+                        {linkedin ? linkedin : "Your linkedIn"}
+                      </b>
                     </p>
                   </td>
                 </tr>
@@ -193,54 +202,34 @@ function SignatureTemlateL() {
                           </td>
 
                           {/* Facebook */}
-                          <td style={{ paddingRight: "10px" }}>
-                            <div
-                              style={{
-                                width: "22px",
-                                height: "22px",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="#333333"
-                                strokeWidth="1.8"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                          <td style={{ padding: "10px 0px 10px 0px" }}>
+                            <Link href={facebook ? facebook : ''}>
+                              <div
+                                style={{
+                                  width: "22px",
+                                  height: "22px",
+                                  display: "flex",
+                                  alignItems: "center",
+                                  justifyContent: "center",
+                                }}
                               >
-                                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                              </svg>
-                            </div>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="20"
+                                  height="20"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="#333333"
+                                  strokeWidth="1.8"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                >
+                                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                                </svg>
+                              </div>
+                            </Link>
                           </td>
 
-                          {/* Pinterest */}
-                          <td>
-                            <div
-                              style={{
-                                width: "22px",
-                                height: "22px",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="#333333"
-                              >
-                                <path d="M12 0C5.373 0 0 5.373 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738a.36.36 0 0 1 .083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.632-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.812 10.763 24 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0z" />
-                              </svg>
-                            </div>
-                          </td>
                         </tr>
                       </tbody>
                     </table>
