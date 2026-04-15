596860e8dc316ece4bd3771eadb1a0be9e452575
Hemlata <payal.gswebtech@gmail.com>
2026-04-15 12:59:30 +0530

    Added DNS overview Section

--- src/app/dns-checker/page.jsx
+++ src/app/dns-checker/page.jsx
@@ -1,11 +1,13 @@
 
+import DnsOverview from '@/components/dns-checker/DnsOverview';
 import Form from './Form';
 
 
 export default function DnsCheckerPage() {
     return (
-        <div>
+        <div className='grid grid-col-1 gap-2'>
            <Form />
+           <DnsOverview />
         </div>
     );
 }
