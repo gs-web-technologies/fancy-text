fa5c4bd87b9e01ea534d3c2799d39df8be5db7db
Hemlata <payal.gswebtech@gmail.com>
2026-04-15 14:56:19 +0530

    Add dns-checker link in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="grid grid-cols-3 gap-10 my-auto mx-auto w-5/6">
 
             {/* Card 1 */}
-            <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition"> 
+            <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition">
                 <h3 className="text-xl font-semibold text-gray-900 mb-2">
                     Text Generator
                 </h3>
@@ -14,11 +14,11 @@ export default function Home() {
                     Generate high-quality fancy text.
                 </p>
                 <Link href="/text-generator" className="btn px-4 py-2 bg-indigo-600 text-white rounded-lg text-sm font-medium hover:bg-red-500 transition">
-                   Generate Text
+                    Generate Text
                 </Link>
             </div>
 
-            {/* Card 3 */}
+            {/* Card 2 */}
             <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition">
                 <h3 className="text-xl font-semibold text-gray-900 mb-2">
                     Email Signature
@@ -27,11 +27,11 @@ export default function Home() {
                     Build professional email signatures for Gmail, Outlook, and more.
                 </p>
                 <Link href="/email-signature?type=1" className="btn px-4 py-2 bg-indigo-600 text-white rounded-lg text-sm font-medium hover:bg-indigo-700 transition">
-                   Create Email Signature
+                    Create Email Signature
                 </Link>
             </div>
 
-            {/* Card 2 */}
+            {/* Card 3 */}
             <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition">
                 <h3 className="text-xl font-semibold text-gray-900 mb-2">
                     Visiting Card
@@ -41,6 +41,20 @@ export default function Home() {
                 </p>
                 <Link href="/email-signature?type=2" className="btn px-4 py-2 bg-indigo-600 text-white rounded-lg text-sm font-medium hover:bg-indigo-700 transition">
                     Create Card
+                </Link>
+            </div>
+
+
+            {/* Card 4 */}
+            <div className="bg-white p-6 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition">
+                <h3 className="text-xl font-semibold text-gray-900 mb-2">
+                    Dns Checker
+                </h3>
+                <p className="text-gray-600 mb-4">
+                    Monitor how your domain resolves across the globe with real-time DNS insights.
+                </p>
+                <Link href="/dns-checker" className="btn px-4 py-2 bg-indigo-600 text-white rounded-lg text-sm font-medium hover:bg-indigo-700 transition">
+                    Check DNS
                 </Link>
             </div>
 
